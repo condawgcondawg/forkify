17c6ff864f1f627b86651ea919807ba9cf46179f
condawgcondawg <connor@nunya.co.uk>
2021-02-23 14:36:04 +0000

    Added new welcome feature

--- src/js/controller.js
+++ src/js/controller.js
@@ -100,6 +100,10 @@ const controlAddRecipe = async function (newRecipe) {
   }
 };
 
+const newFeature = function () {
+  console.log('Welcome to the application!');
+};
+
 const init = function () {
   bookmarksView.addHandlerRender(controlBookmarks);
   recipeView.addHandlerRender(controlRecipes);
@@ -108,5 +112,6 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
+  newFeature();
 };
 init();
