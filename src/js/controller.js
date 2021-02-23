2a02d92f34b0f00860557ca6130930b5abc1f4ef
condawgcondawg <connor@nunya.co.uk>
2021-02-23 15:03:24 +0000

    Cleaning controller

--- src/js/controller.js
+++ src/js/controller.js
@@ -100,10 +100,6 @@ const controlAddRecipe = async function (newRecipe) {
   }
 };
 
-const newFeature = function () {
-  console.log('Welcome to the application!');
-};
-
 const init = function () {
   bookmarksView.addHandlerRender(controlBookmarks);
   recipeView.addHandlerRender(controlRecipes);
@@ -112,6 +108,5 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
-  newFeature();
 };
 init();
